1a1e2a6f5556478df4a6e236df8caa266abfa652
lilzy218 <lily.schroder@gmail.com>
2025-07-03 13:10:47 +0100

    integrated API to include current forecast include min/max temp + icon

--- src/app.js
+++ src/app.js
@@ -50,32 +50,44 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
 function getForecast(city) {
   let apiKey = "4a90co310af353688etc1f6b420b6eed";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
   axios(apiUrl).then(displayForecast);
-  console.log(apiUrl);
 }
 
 function displayForecast(response) {
   console.log(response.data);
 
-  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
   let forecastHtml = "";
 
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
   <div class="weather-forecast-days">
-            <div class="weather-forecast-day">${day}</div>
-            <div class="weather-forecast-icon">🌤️</div>
+            <div class="weather-forecast-day">${formatDay(day.time)}</div>
+      
+            <img src="${day.condition.icon_url}" class="weather-forecast-icon"/>
             <div class="weather-forecast-temps">
-              <div class="temp-high">15°C</div>
-              <div class="temp-low">9°C</div>
+              <div class="temp-high">${Math.round(
+                day.temperature.maximum
+              )}°C</div>
+              <div class="temp-low">${Math.round(
+                day.temperature.minimum
+              )}°C</div>
             </div>
           </div>
           `;
+    }
   });
 
   let forecastElement = document.querySelector("#forecast");
